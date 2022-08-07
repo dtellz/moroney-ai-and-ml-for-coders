ccdcf3c7ccce06d5572b4d0daed4b15960b74c7e
Diego <dtellez95@gmail.com>
2022-08-07 13:01:32 +0100

    add comments

--- scripts/cp_1.py
+++ scripts/cp_1.py
@@ -3,7 +3,8 @@ import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 
-model = Sequential([Dense(units=1, input_shape=[1])]) #define the layer and its neurons. In this case 1 layer with 1 neuron
+l0 = Dense(units=1, input_shape=[1]) #Define layers with 1 neuron
+model = Sequential([l0]) #Define the model. In this case 1 layer with 1 neuron
 model.compile(optimizer='sgd', loss='mean_squared_error') #here is where the heavy calculus ops are execution behind the scenes. Abstract to us
 
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float) # x= -1, 0, 1, 2, 3, 4
@@ -12,5 +13,5 @@ ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float) # y= -3, -1, 1, 3, 
 model.fit(xs, ys, epochs=500) # This is the neural network learning process
 
 print(model.predict([10, 0])) # Asking the model to predict what will be the value of Y when X is 10
-
 # Output [[18.97963  ][-0.9908468]]
+print("Model learned (layer 0): {}".format(l0.get_weights()))
